8655301bd8d2368d13a9e547920c1fc7346efae7
Александр Гузий <aleksandrguzij@MacBook-Pro-Aleksandr.local>
2023-08-11 17:05:08 +0600

    Добавлен тест для assignAircraftsInspection из AircraftsService

--- src/test/java/kg/airport/airportproject/service/AircraftsServiceTest.java
+++ src/test/java/kg/airport/airportproject/service/AircraftsServiceTest.java
@@ -3,17 +3,19 @@ package kg.airport.airportproject.service;
 import kg.airport.airportproject.configuration.SecurityConfigurationTest;
 import kg.airport.airportproject.dto.AircraftRequestDto;
 import kg.airport.airportproject.dto.AircraftResponseDto;
-import kg.airport.airportproject.entity.AircraftsEntity;
-import kg.airport.airportproject.entity.PartsEntity;
+import kg.airport.airportproject.entity.*;
 import kg.airport.airportproject.entity.attributes.AircraftStatus;
 import kg.airport.airportproject.entity.attributes.AircraftType;
 import kg.airport.airportproject.entity.attributes.PartType;
 import kg.airport.airportproject.repository.AircraftsEntityRepository;
+import kg.airport.airportproject.repository.ApplicationUsersEntityRepository;
 import kg.airport.airportproject.repository.PartsEntityRepository;
+import kg.airport.airportproject.response.StatusChangedResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
@@ -32,6 +34,10 @@ public class AircraftsServiceTest {
     private AircraftsEntityRepository aircraftsEntityRepository;
     @Autowired
     private PartsEntityRepository partsEntityRepository;
+    @Autowired
+    private ApplicationUsersEntityRepository applicationUsersEntityRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Test
     public void testRegisterNewAircraft_OK() {
         List<PartsEntity> partsEntities = this.createTestAircraftParts();
@@ -60,6 +66,27 @@ public class AircraftsServiceTest {
         }
     }
 
+    // TODO: 11.08.2023 Протестировать методы требующие аутентикации во время тестирования контроллеров
+
+    @Test
+    public void testAssignAircraftsInspection_OK() {
+        AircraftsEntity aircraft = this.createAircraftsEntity(AircraftStatus.NEEDS_INSPECTION);
+        aircraft = this.aircraftsEntityRepository.save(aircraft);
+
+        ApplicationUsersEntity engineer = this.createEngineersEntityByParameters("test", "test");
+        engineer = this.applicationUsersEntityRepository.save(engineer);
+        try {
+            StatusChangedResponse statusChangedResponse =
+                    this.aircraftsService.assignAircraftInspection(aircraft.getId(), engineer.getId());
+            Assertions.assertTrue(statusChangedResponse.getMessage().endsWith("[ON_INSPECTION]"));
+
+            AircraftsEntity resultEntity = this.aircraftsService.findAircraftsEntityById(aircraft.getId());
+            Assertions.assertEquals(engineer.getId(), resultEntity.getServicedBy().getId());
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
     private List<PartsEntity> createTestAircraftParts() {
         List<PartsEntity> partsEntities = new ArrayList<>();
         partsEntities.add(
@@ -81,5 +108,27 @@ public class AircraftsServiceTest {
                         .setPartType(PartType.WING_PART)
         );
         return this.partsEntityRepository.saveAll(partsEntities);
+    }
+
+    private ApplicationUsersEntity createEngineersEntityByParameters(
+            String username,
+            String fullName
+    ) {
+        ApplicationUsersEntity applicationUsersEntity = new ApplicationUsersEntity()
+                .setUsername(username)
+                .setFullName(fullName)
+                .setPassword(this.passwordEncoder.encode("test"))
+                .setUserPosition(new UserPositionsEntity().setId(6L).setPositionTitle("ENGINEER"));
+
+        applicationUsersEntity.getUserRolesEntityList().add(new UserRolesEntity().setId(7L).setRoleTitle("ENGINEER"));
+
+        return applicationUsersEntity;
+    }
+
+    private AircraftsEntity createAircraftsEntity(AircraftStatus status) {
+        return new AircraftsEntity()
+                .setAircraftType(AircraftType.PLANE)
+                .setStatus(status)
+                .setTitle("test");
     }
 }
