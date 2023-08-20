5aeb3c83cd0a523035b6efb02b85d02eb5b44fa6
Александр Гузий <aleksandrguzij@MacBook-Pro-Aleksandr.local>
2023-08-20 18:06:09 +0600

    Добавлен тест для generateAircraftSeats

--- src/test/java/kg/airport/airportproject/service/AircraftSeatsServiceTest.java
+++ src/test/java/kg/airport/airportproject/service/AircraftSeatsServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -139,6 +140,54 @@ public class AircraftSeatsServiceTest {
                 () -> this.aircraftSeatsService.cancelSeatReservation(aircraftSeatsEntity.getId()),
                 String.format("Ошибка! Место с ID [%d] свободно!", aircraftSeatsEntity.getId())
         );
+    }
 
+    @Test
+    public void testGenerateAircraftSeats_OK() {
+        Integer rowsNumber = 2;
+        Integer numberOfSeatsInRow = 2;
+        try {
+            List<AircraftSeatsEntity> resultList =
+                    this.aircraftSeatsService.generateAircraftSeats(rowsNumber, numberOfSeatsInRow);
+
+            for (int i = 0; i < resultList.size(); i++) {
+                Assertions.assertEquals(
+                        this.evaluateRowNumber(rowsNumber, numberOfSeatsInRow, i),
+                        resultList.get(i).getRowNumber()
+                );
+                Assertions.assertEquals(
+                        this.evaluateNumberOfSeatInRow(numberOfSeatsInRow, i),
+                        resultList.get(i).getNumberInRow()
+                );
+            }
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testGenerateAircraftSeats_NullRowsNumber() {
+        Assertions.assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> this.aircraftSeatsService.generateAircraftSeats(null, 1),
+                "Количество рядов и количество мест в ряду не может быть null!"
+        );
+    }
+
+    @Test
+    public void testGenerateAircraftSeats_RowsNumberLessThanOne() {
+        Assertions.assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> this.aircraftSeatsService.generateAircraftSeats(0, 1),
+                "Количество рядов и количество мест в ряду не может быть меньше 1!"
+        );
+    }
+
+    private Integer evaluateRowNumber(Integer rowsNumber, Integer numberOfSeatsInRow, int index) {
+        return rowsNumber - (rowsNumber - (index / numberOfSeatsInRow + 1));
+    }
+
+    private Integer evaluateNumberOfSeatInRow(Integer numberOfSeatsInRow, int index) {
+        return (index % numberOfSeatsInRow) + 1;
     }
 }
